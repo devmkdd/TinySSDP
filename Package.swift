003486e1a848a3d2b13d246c94728215dfee6ce2
Michael <dev.mkdd@gmail.com>
2020-12-29 10:59:55 +0100

    feat(decrease_to_v11): re-activate mac

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "TinySSDP",
     platforms: [
-           SupportedPlatform.iOS(.v11)
+        SupportedPlatform.iOS(.v11),
+        SupportedPlatform.macOS(.v10_14)
+        
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
